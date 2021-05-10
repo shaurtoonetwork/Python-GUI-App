c53b0bfabd07c92ce57536ae752cf000c1917f35
shaurtoonetwork <chaturvedi.shaurya7@gmail.com>
2021-05-10 16:37:44 +0530

    Final Commit

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import tkinter as tk
-from tkinter import filedialog,Text
+from tkinter import filedialog, Text
 import os
 
 root = tk.Tk()
 apps = []
+
+if os.path.isfile('dave.txt'):
+    with open('save.txt','r') as f:
+        tempApps = f.read()
+        tempApps = tempApps.split(',')
+        apps = [x for x in tempApps if x.strip()]
 
 
 def addApps():
@@ -21,7 +27,9 @@ def addApps():
 
 def runApps():
     for app in apps:
-        os.start(app)
+        os.startfile(app)
+
+
 
 
 canvas = tk.Canvas(root,height = 700,width = 700, bg="#263D42")
@@ -33,10 +41,20 @@ frame.place(relwidth = 0.8,relheight = 0.8,relx=0.1, rely = 0.1)
 openFile = tk.Button(root, text = "Open File",padx=10, pady=5, fg="white",bg="#263D42", command=addApps)
 openFile.pack()
 
-runApps = tk.Button(root, text = "Run Apps",padx=10, pady=5, fg="white",bg="#263D42",command=runApps)
+runApps = tk.Button(root, text = "Run Apps",padx=10, pady=5, fg="white",bg="#263D42", command = runApps)
 runApps.pack()
 
+
+for app in apps:
+    label = tk.Label(frame, text=app)
+    label.pack()
+    
 root.mainloop()
+
+with open('save.txt','w') as f:
+    for app in apps:
+        f.write(app + ',')
+
 
 
 
