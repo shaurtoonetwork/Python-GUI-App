f4534b7a108099af206ab73afedf9c1a222c8f0c
shaurtoonetwork <chaturvedi.shaurya7@gmail.com>
2021-05-10 15:39:36 +0530

    First Commit

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print(2)
+import tkinter as tk
+from tkinter import filedialog,Text
+import os
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root,height = 700,width = 700, bg="#263D42")
+canvas.pack()
+
+
+root.mainloop()
+
+
+
+
+
